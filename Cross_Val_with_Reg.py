f63bd0833326ea015e86a27f7c5ca3d056f48f12
Peng Gu <pgu@oberlin.edu>
2020-02-25 21:59:52 -0500

    some changes

--- Cross_Val_with_Reg.py
+++ Cross_Val_with_Reg.py
@@ -16,32 +16,75 @@ import math
 def mse(target, prediction):
     return (np.square(target - prediction)).mean(axis=None)
 
+
 # (b)
-def cross_val(t, X, K, seed=1, lam=0, train_err=False):
-    total = np.shape(X)[0]
+def cross_val(ordinarydata, K, seed=1, lam=0, train_err=False):
+    total = np.shape(ordinarydata)[0]
     if total == 0:
         print("dataset is empty - abort!")
         return
     np.random.seed(seed)
-    shuffled_set = np.copy(X)
+    shuffled_set = np.copy(ordinarydata)
     np.random.shuffle(shuffled_set)
     mse_list = []
     ret = []
-    for i in range():
+    for i in range(K):
         start = math.floor(i * total / K)
         end = math.floor((i + 1) * total / K)
         val_set = shuffled_set[start:end]
         train_set = np.concatenate((shuffled_set[:start], shuffled_set[end:]))
-        ret.append(reg.getOLSpoly(train_set, t, k))
-    return ret
+        W = reg.getOLS(train_set, lam)
+        ret.append(W)
+        pred = np.dot(val_set[:, :-1], np.transpose(W))
+        mse = reg.mse(pred, val_set[:, -1])
+        mse_list.append(mse)
+    return np.mean(mse_list), np.std(mse_list)
 
-def best_order(target, predictor, K, seed, ret_err=False, D=None):
-    if D==None:
-        D = np.shape(target)[0]
+
+# (c)
+def best_order(ordinarydata, K, seed, ret_err=False, D=None):
+    if D is None:
+        D = np.shape(ordinarydata)[0]
+    total = np.shape(ordinarydata)[0]
+    if total == 0:
+        print("dataset is empty - abort!")
+        return
+    if K < 2:
+        print("K too small")
+        return
+    np.random.seed(seed)
+    shuffled_set = np.copy(ordinarydata)
+    np.random.shuffle(shuffled_set)
+    KD_matrix = np.empty((K, D))
+    KD_matrix_train = np.empty((K, D))
+    for i in range(K):
+        start = math.floor(i * total / K)
+        end = math.floor((i + 1) * total / K)
+        val_set = shuffled_set[start:end]
+        train_set = np.concatenate((shuffled_set[:start], shuffled_set[end:]))
+        for j in range(D+1):
+            W = reg.getOLSpoly(train_set, D)
+            pred = np.dot(val_set[:, :-1], np.transpose(W))
+            mse = reg.mse(pred, val_set[:, -1])
+            KD_matrix[i,j]=mse
+            if ret_err is True:
+                pred_train = np.dot(train_set[:, :-1], np.transpose(W))
+                KD_matrix_train[i, j]=reg.mse(pred_train, train_set[:, -1])
+    means = np.mean(KD_matrix, axis=0)
+    sds = np.std(KD_matrix, axis=0)
+    best_index = np.argmin(means)
+    if ret_err is True:
+        return means, sds, best_index
+    else:
+        return means, best_index
+
 
 
 def main():
-    reg.getdataset("womens100.csv")
+    data = reg.getdataset("womens100.csv")
+    means, best_index = best_order(data, 10, 1)
+
+
 
 if __name__ == "__main__":
     main()
